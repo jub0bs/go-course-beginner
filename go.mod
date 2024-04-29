e5bf603a9ee14e33ef54bbf6a9736c05c0c4ef32
jub0bs <jcretel-infosec+github@protonmail.com>
2024-04-29 19:44:25 +0200

    migrate from jub0bs/fcors to jub0bs/cors

--- go.mod
+++ go.mod
@@ -1,13 +1,15 @@
 module whatever
 
-go 1.21
+go 1.22
+
+toolchain go1.22.2
 
 require (
-	github.com/jub0bs/fcors v0.6.0
+	github.com/jub0bs/cors v0.1.2
 	golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa
 )
 
 require (
-	golang.org/x/net v0.18.0 // indirect
+	golang.org/x/net v0.23.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
--- src/cors.go
+++ src/cors.go
@@ -5,32 +5,33 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/jub0bs/fcors"
+	"github.com/jub0bs/cors"
 )
 
-func hello(w http.ResponseWriter, _ *http.Request) {
+func handleHello(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	io.WriteString(w, `{"hello":"world"}`)
 }
 
 // START OMIT
 func main() {
-	// register the hello handler for the /hello path
-	http.HandleFunc("/hello", hello)
+	// create a custom ServeMux and register your handler to pattern GET /hello
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /hello", handleHello)
 
 	// instantiate a CORS middleware whose config suits your needs
-	cors, err := fcors.AllowAccess( // HL
-		fcors.FromAnyOrigin(), // HL
-	) // HL
+	corsMw, err := cors.NewMiddleware(cors.Config{ // HL
+		Origins: []string{"*"}, // HL
+	}) // HL
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// apply the CORS middleware to the default ServeMux
-	mux := cors(http.DefaultServeMux) // HL
+	// apply the CORS middleware
+	handler := corsMw.Wrap(mux) // HL
 
-	// start the server on port 8080; make sure to path the modified ServeMux
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	// start the server on port 8080; make sure to pass your custom handler
+	if err := http.ListenAndServe(":8080", handler); err != nil {
 		log.Fatal(err)
 	}
 }
